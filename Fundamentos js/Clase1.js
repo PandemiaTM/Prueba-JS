33bf723c852d689940cf09f3a779917d5c2a6381
Miguel larios <78969984+PandemiaTM@users.noreply.github.com>
2021-02-22 17:09:32 -0500

    Add files via upload

---
+++ Fundamentos js/Clase1.js
@@ -0,0 +1,11 @@
+var nombre = 'miguellariosriuz', apellido = 'larios'
+
+var nombreMY = nombre.toUpperCase()
+var apellidoML = apellido.toLocaleLowerCase()
+
+var primeraLetra = apellido.charAt(0)
+var cantidadDeLetras = nombre.length
+
+var nombreCompleto = `${nombre} ${apellido.toUpperCase()}`
+
+var str = nombre.substr(5,4)
